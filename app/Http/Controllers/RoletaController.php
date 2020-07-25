f1eeaf94492cc82ef89e10be9db1530cb611340f
FilipiLira <filipe19.fc@gmail.com>
2020-07-25 00:19:56 -0300

    'fristComit'

---
+++ app/Http/Controllers/RoletaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RoletaController extends Controller
+{
+    public function renderRoleta(){
+
+        return view('index');
+    }
+}
